86549bc15570acb28cbf03a7c6df26f6b1149aec
zhi_hao_ong <zhi_hao_ong@aiap.sg>
2024-01-08 22:37:56 +0800

    minor changes

--- asr/api-entrypoint.sh
+++ asr/api-entrypoint.sh
@@ -12,14 +12,14 @@ cd ..
 echo "PWD: $PWD"
 
 # Instructions for user
-# echo "================================================================================================================================================"
-# echo "After seeing the messages 'Application startup complete' for the FASTAPI UvicornWorkers,
-# pls proceed to use FASTAPI Swagger UI to test the Upload json file feature /api/v1/model/upload/ endpoint at http://localhost:8080/docs"
-# echo "You may also use the CURL command to use the FAST API /api/v1/model/predict endpoint for batch inference"
-# echo "================================================================================================================================================"
+echo "============================================================================================="
+echo "After seeing the messages 'Application startup complete' for the FASTAPI UvicornWorkers,
+pls proceed to test the server is running with 'curl http://localhost:8001/ping'
+You should receive a pong response"
+echo "You may also use the CURL command for inference: 
+'curl -F file=@path/to/sample-000000.mp3 http://localhost:8001/asr'"
+echo "============================================================================================="
 
-# gunicorn vits_fastapi.main:APP -b 0.0.0.0:8080 -w $NUM_WORKERS -k uvicorn.workers.UvicornWorker --timeout $TIMEOUT_DURATION
-# gunicorn lightspeed_fastapi.main:APP -b 0.0.0.0:8080 -w 4 -k uvicorn.workers.UvicornWorker --timeout 90
 gunicorn -w 2 -k uvicorn.workers.UvicornWorker asr.asr_api:app --bind 0.0.0.0:8001 --timeout=300
 
 ## Alternatively:

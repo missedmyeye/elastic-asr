3482b57140df22c5fe1e6fe3a956049a29b5ec2f
zhi_hao_ong <zhi_hao_ong@aiap.sg>
2024-01-10 13:52:33 +0800

    elastic-backend set up

---
+++ elastic-backend/cv-index.py
@@ -0,0 +1,41 @@
+from elasticsearch import Elasticsearch
+import pandas as pd
+from tqdm import tqdm
+
+# Function to wait for Elasticsearch to start
+def wait_for_elasticsearch():
+    while True:
+        # Elasticsearch configuration
+        es = Elasticsearch(['http://es01:9200'])
+        status = es.ping()
+        if status:
+            print("Connected to Elasticsearch.")
+            return es
+        else:
+            print("Connection failed.")
+            raise Exception("Connection failed.")
+
+# Wait for Elasticsearch to start before proceeding
+es = wait_for_elasticsearch()
+
+# Indexing function
+def index_data(index_name, data_frame, es):
+    for _, row in tqdm(
+        data_frame.iterrows(), desc="Processing rows", total=len(data_frame), unit="row"
+        ):
+        document = row.to_dict()
+        # print(document)
+        es.index(index=index_name, body=document)
+    print("Indexing complete.")
+
+# Read CSV file
+csv_file = 'cv-valid-dev.csv'
+df = pd.read_csv(csv_file)
+# Replace null values with an empty string
+# Elasticsearch is unable to parse null values
+df = df.fillna('')
+print(f'Reading CSV...{csv_file}')
+print(df.head())
+# Index data into Elasticsearch
+index_name = 'cv-transcriptions'
+index_data(index_name, df, es)
